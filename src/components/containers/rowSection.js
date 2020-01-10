9a2acd5ba53ec98d44c5562155d0e2e0b21c10dd
Montensia Banks <3188572+Monty7@users.noreply.github.com>
2020-01-09 19:59:57 -0600

    Created a container for the row of articles

---
+++ src/components/containers/rowSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ArticleProject from '../articleProject'
+
+const RowSection = () => {
+
+    return (
+        <section>
+            <ArticleProject />
+        </section>
+    )
+}
