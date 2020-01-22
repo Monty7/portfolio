ee6ebd405ab51a6cb156e9d7c19d3d8796a1bf3d
Montensia Banks <3188572+Monty7@users.noreply.github.com>
2020-01-21 20:57:52 -0600

    Updated css to fix project name layout

--- src/components/sectionNames.js
+++ src/components/sectionNames.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SectionName = (props) => {
     return(
-        <div>
+        <div className="project_name">
             <h2>{props.orgName}</h2>
         </div>
     )
